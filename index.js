4a583818434aa8e7f87d492da356d2b6d861405a
charles yarney <charlesyarney@gmail.com>
2021-10-06 18:56:55 +0000

    complete both proplems

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 //1
-function absoluteValue(){
+// function absoluteValue(num){
+//   if( num > 0){
+//     return num;
+//   }else if (num < 0){
+//     return num * -1;
+//   }else
+//     return "null";
+//}
+console.log(absoluteValue("13"))
+
+
+function absoluteValue(num){   
+  if (typeof num === "number"){
+      if(num < 0){
+        return num * -1
+        }else{
+        return num
+        }
+  }else{
+     return null
+  }
   
 }
-
-
-
-
+//bonus
+function multiplesOfFourAndSix(){
+  for(let i = 0; i <= 100; i++){
+    if(i % 4 ==0 || i % 6 ==0){
+        console.log(i);
+    }
+ }
+}
+//multiplesOfFourAndSix()
